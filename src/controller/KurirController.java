6a65be7d4505091e63b5b02cf8087bbff3513240
HUDA26122022 <muliahudaahmad@gmail.com>
2024-12-23 18:44:02 +0700

    menambahkan fitur tambah, update, delete

--- src/controller/KurirController.java
+++ src/controller/KurirController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.*;
 import java.util.List;
@@ -25,7 +27,16 @@ public class KurirController {
     public List<Kurir> getAllKurir() {
         try (SqlSession session = sqlSessionFactory.openSession()) {
             KurirMapper mapper = session.getMapper(KurirMapper.class);
-            return mapper.getAllKurir();
+            List<Kurir> kurirList = mapper.getAllKurir();
+
+            // Format tanggal lahir menjadi yyyy-MM-dd
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            for (Kurir kurir : kurirList) {
+                if (kurir.getTanggalLahir() != null) {
+                    kurir.setTanggalLahir(java.sql.Date.valueOf(dateFormat.format(kurir.getTanggalLahir())));
+                }
+            }
+            return kurirList;
         }
     }
 
@@ -33,6 +44,36 @@ public class KurirController {
         try (SqlSession session = sqlSessionFactory.openSession()) {
             KurirMapper mapper = session.getMapper(KurirMapper.class);
             return mapper.getKurirById(id);
+        }
+    }
+
+    public void addKurir(Kurir kurir) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            KurirMapper mapper = session.getMapper(KurirMapper.class);
+            mapper.insertKurir(kurir);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateKurir(Kurir kurir) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            KurirMapper mapper = session.getMapper(KurirMapper.class);
+            mapper.updateKurir(kurir);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteKurir(int id) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            KurirMapper mapper = session.getMapper(KurirMapper.class);
+            mapper.deleteKurir(id);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
