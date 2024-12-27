1a4351a66db28bec041c11266839eb9f92fa9c53
Novan <novanramdan7@gmail.com>
2024-12-27 16:25:28 +0700

    Fitur Pengelolaan sampah

--- src/model/sampahMapper.java
+++ src/model/sampahMapper.java
@@ -6,11 +6,11 @@ import org.apache.ibatis.annotations.*;
 public interface sampahMapper {
 
     // CREATE: Menambahkan data baru ke tabel sampah
-    @Insert("INSERT INTO sampah (idSampah, kategoriSampah, berat, poin, idMasyarakat, idTps, idKurir) VALUES (#{idSampah}, #{kategoriSampah}, #{berat}, #{poin}, #{idMasyarakat}, #{idTps}, #{idKurir})")
+    @Insert("INSERT INTO sampah (idSampah, kategoriSampah, jenisSampah, idTps) VALUES (#{idSampah}, #{kategoriSampah}, #{jenisSampah}, #{idTps})")
     void insertSampah(Sampah sampah);
 
-    // READ: Mengambil semua data dari tabel sampah
-    @Select("SELECT * FROM sampah")
+    // READ: Mengambil semua data sampah
+    @Select("SELECT idSampah, kategoriSampah, jenisSampah, idTps FROM sampah")
     List<Sampah> getAllSampah();
 
     // READ: Mengambil data sampah berdasarkan ID
@@ -18,10 +18,14 @@ public interface sampahMapper {
     Sampah getSampahById(@Param("idSampah") int idSampah);
 
     // UPDATE: Memperbarui data sampah berdasarkan ID
-    @Update("UPDATE sampah SET kategoriSampah = #{kategoriSampah}, berat = #{berat}, poin = #{poin}, idMasyarakat = #{idMasyarakat}, idTps = #{idTps}, idKurir = #{idKurir} WHERE idSampah = #{idSampah}")
+    @Update("UPDATE sampah SET kategoriSampah = #{kategoriSampah}, jenisSampah = #{jenisSampah}, idTps = #{idTps} WHERE idSampah = #{idSampah}")
     void updateSampah(Sampah sampah);
 
     // DELETE: Menghapus data sampah berdasarkan ID
     @Delete("DELETE FROM sampah WHERE idSampah = #{idSampah}")
     void deleteSampah(@Param("idSampah") int idSampah);
+
+    // NEW: Mengambil daftar ID TPS dari tabel TPS
+    @Select("SELECT idTps FROM dropbox")
+    List<Integer> getAllTpsIds();
 }
